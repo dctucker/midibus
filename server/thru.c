8f71d4b4b4a1643b5a00939da6e65c17e8e8de23
Casey Tucker <dctucker@hotmail.com>
2018-12-10 07:29:32 +0000

    threaded midi thru

--- server/thru.c
+++ server/thru.c
@@ -1,11 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <alsa/asoundlib.h>
-#include <signal.h>
-
-static snd_rawmidi_t *input, **inputp;
-static snd_rawmidi_t *output, **outputp;
+#include "thru.h"
 
 static void error(const char *format, ...)
 {
@@ -17,36 +10,31 @@ static void error(const char *format, ...)
 	putc('\n', stderr);
 }
 
-int stop = 0;
-
 void sig_handler(int sig)
 {
-	stop = 1;
+	stop_all = 1;
 }
 
-int main(int argc, char **argv)
+void *read_thread(void *arg)
 {
 	int err;
-	char *port_in  = "hw:JUNODS";
-	char *port_out = "hw:Deluge";
+	struct read_thread_data *data = arg;
 
-	inputp = &input;
-	outputp = NULL;
-	if ((err = snd_rawmidi_open(inputp, outputp, port_in, SND_RAWMIDI_NONBLOCK)) < 0) {
-		//error("cannot open port \"%s\": %s", port_in, snd_strerror(err));
+	if ((err = snd_rawmidi_open(&data->midi, NULL, data->port_name, SND_RAWMIDI_NONBLOCK)) < 0) {
+		error("cannot open port \"%s\": %s", data->port_name, snd_strerror(err));
 		goto cleanup;
 	}
+	printf("Opened %s for read\n", data->port_name);
 
-	snd_rawmidi_read(input, NULL, 0); // trigger reading
+	snd_rawmidi_read(data->midi, NULL, 0); // trigger reading
 
 	int read = 0;
 	int npfds, time = 0;
 	struct pollfd *pfds;
 
-	npfds = snd_rawmidi_poll_descriptors_count(input);
+	npfds = snd_rawmidi_poll_descriptors_count(data->midi);
 	pfds = alloca(npfds * sizeof(struct pollfd));
-	snd_rawmidi_poll_descriptors(input, pfds, npfds);
-	signal(SIGINT, sig_handler);
+	snd_rawmidi_poll_descriptors(data->midi, pfds, npfds);
 
 	do
 	{
@@ -56,35 +44,92 @@ int main(int argc, char **argv)
 
 		err = poll(pfds, npfds, 200);
 		if (err < 0) {
-			if( ! stop )
+			if( ! stop_all )
 				error("poll failed: %s", strerror(errno));
 			break;
 		}
-		if ((err = snd_rawmidi_poll_descriptors_revents(input, pfds, npfds, &revents)) < 0) {
+		if ((err = snd_rawmidi_poll_descriptors_revents(data->midi, pfds, npfds, &revents)) < 0) {
 			error("cannot get poll events: %s", snd_strerror(errno));
 			break;
 		}
 		if( revents & (POLLERR | POLLHUP ) ) break;
 		if( ! ( revents & POLLIN ) ) continue;
-		err = snd_rawmidi_read( input, buf, sizeof(buf) );
+		err = snd_rawmidi_read( data->midi, buf, sizeof(buf) );
 		if (err == -EAGAIN) continue;
 		if (err < 0) {
-			error("cannot read from port \"%s\": %s", port_in, snd_strerror(err));
+			error("cannot read from port \"%s\": %s", data->port_name, snd_strerror(err));
 			break;
 		}
+
+		for( int o = 0; o < data->n_outs; ++o )
+		{
+			struct write_data *out = &data->outs[o];
+			if( out->midi == NULL || out->func == NULL )
+				continue;
+			out->func( out->midi, buf, err );
+		}
+
 		for (i = 0; i < err; ++i)
 			printf("%02X ", buf[i]);
 		printf("\n");
 		fflush(stdout);
 	}
-	while( ! stop );
+	while( ! stop_all );
 
 cleanup:
-	if (inputp)
-		snd_rawmidi_close(input);
-	if (outputp)
-		snd_rawmidi_close(output);
-	printf("\n");
+	snd_rawmidi_close(data->midi);
+	printf("\nDone.\n");
 	fflush(stderr);
 	fflush(stdout);
 }
+
+int write_thru(snd_rawmidi_t *port, unsigned char *buf, int n_bytes)
+{
+	snd_rawmidi_write( port, buf, n_bytes );
+}
+
+void manage_outputs()
+{
+	printf("Managing outputs\n");
+	for( int i = 0; i < MAX_THREADS; ++i )
+	{
+		struct read_thread_data *in = &read_data[i];
+		if( in->midi == NULL )
+			continue;
+		printf("Checking input %d\n", i);
+		for( int o = 0; o < in->n_outs; ++o )
+		{
+			int err;
+			struct write_data *out = &in->outs[o];
+
+			if( out->midi != NULL ) continue;
+			if ((err = snd_rawmidi_open(NULL, &out->midi, out->port_name, SND_RAWMIDI_NONBLOCK)) < 0) {
+				error("cannot open port \"%s\": %s", out->port_name, snd_strerror(err));
+				continue;
+			}
+			printf("Connected %s to %s\n", in->port_name, out->port_name);
+		}
+	}
+}
+
+int main(int argc, char **argv)
+{
+	pthread_t threads[MAX_THREADS];
+
+	read_data[0].port_name = "hw:JUNODS";
+	read_data[0].outs[0].port_name = "hw:Deluge";
+	read_data[0].outs[0].midi = NULL;
+	read_data[0].outs[0].func = write_thru;
+	read_data[0].n_outs = 1;
+
+	int i = 0;
+
+	signal(SIGINT, sig_handler);
+	signal(SIGHUP, manage_outputs);
+	pthread_create( &threads[i], NULL, read_thread, (void *) &read_data[0] );
+	sleep(1);
+	manage_outputs();
+	pthread_join( threads[i], NULL);
+	return EXIT_SUCCESS;
+}
+
