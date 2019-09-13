40bea4423d66953e53d364175b66c898014bbe55
Casey Tucker <dctucker@hotmail.com>
2019-09-13 14:13:12 -0700

    generic DeviceMap

--- rust/src/devices.rs
+++ rust/src/devices.rs
@@ -1,43 +1,47 @@
-use std::io::Read;
+use std::sync::{Arc,RwLock};
+use crate::thru::ReadThread;
+use crate::output::OutputDevice;
+use std::collections::HashMap;
 
-use alsa::rawmidi::Iter as RawMidiIter;
-use alsa::Direction;
-use alsa::rawmidi::Rawmidi;
-use alsa::card::Iter as CardIter;
-use alsa::ctl::Ctl;
+pub trait Create<T> {
+	fn create( key : &String ) -> T;
+}
 
-pub fn list_cards() {
-	let cards = CardIter::new();
-	for (_i,c) in cards.enumerate() {
-		let card = c.unwrap();
-		println!("{} \"{}\":", card.get_longname().unwrap(), card.get_name().unwrap());
-		let ctl = Ctl::from_card(&card, true).unwrap();
-		let raws = RawMidiIter::new(&ctl);
-		for (_i,r) in raws.enumerate() {
-			let raw = r.unwrap();
-			println!("\t{}: {} ({:?})",
-				raw.get_id().unwrap(),
-				raw.get_subdevice_name().unwrap(),
-				raw.get_stream()
-			);
+#[derive(Debug)]
+pub struct DeviceMap<T> {
+	map : HashMap<String,T>
+}
+impl<T> DeviceMap<T> {
+	pub fn new() -> DeviceMap<T> { DeviceMap { map: HashMap::new() } }
+}
+impl<T> std::ops::Deref for DeviceMap<T> {
+	type Target = HashMap<String,T>;
+	fn deref(&self) -> &Self::Target { &self.map }
+}
+impl<T> std::ops::DerefMut for DeviceMap<T> {
+	fn deref_mut(&mut self) -> &mut Self::Target { &mut self.map }
+}
+impl<T> DeviceMap<T> where DeviceMap<T> : Create<T> {
+	pub fn by_name( &mut self, key : &String ) -> &T {
+		if ! self.contains_key(key) {
+			self.insert(String::from(key), Self::create(key));
 		}
+		self.get(key).unwrap()
 	}
 }
 
-pub fn main() {
-	/*
-	use std::ffi::CString;
-	use alsa::device_name::HintIter;
-
-	for t in &["rawmidi"] {
-		println!("{} devices:", t);
-		let i = HintIter::new(None, &*CString::new(*t).unwrap()).unwrap();
-		for a in i { println!("  {:?}", a) }
-	}
-	list_cards();
-	*/
-	let mio = Rawmidi::new("hw:mio", Direction::input(), false).unwrap();
-	for data in mio.io().bytes() {
-		println!("{}", data.unwrap());
+pub type ReadThreadMap = DeviceMap<ReadThread>;
+impl Create<ReadThread> for ReadThreadMap {
+	fn create( key : &String ) -> ReadThread {
+		println!("New input device {}", key);
+		ReadThread::new(String::from(key))
 	}
 }
+
+pub type OutputDeviceMap = DeviceMap<Arc<RwLock<OutputDevice>>>;
+impl Create<Arc<RwLock<OutputDevice>>> for OutputDeviceMap {
+	fn create( key : &String ) -> Arc<RwLock<OutputDevice>> {
+		Arc::new(RwLock::new(OutputDevice::new(key.clone())))
+	}
+}
+
---
+++ rust/src/list.rs
@@ -0,0 +1,43 @@
+use std::io::Read;
+
+use alsa::rawmidi::Iter as RawMidiIter;
+use alsa::Direction;
+use alsa::rawmidi::Rawmidi;
+use alsa::card::Iter as CardIter;
+use alsa::ctl::Ctl;
+
+pub fn list_cards() {
+	let cards = CardIter::new();
+	for (_i,c) in cards.enumerate() {
+		let card = c.unwrap();
+		println!("{} \"{}\":", card.get_longname().unwrap(), card.get_name().unwrap());
+		let ctl = Ctl::from_card(&card, true).unwrap();
+		let raws = RawMidiIter::new(&ctl);
+		for (_i,r) in raws.enumerate() {
+			let raw = r.unwrap();
+			println!("\t{}: {} ({:?})",
+				raw.get_id().unwrap(),
+				raw.get_subdevice_name().unwrap(),
+				raw.get_stream()
+			);
+		}
+	}
+}
+
+pub fn main() {
+	/*
+	use std::ffi::CString;
+	use alsa::device_name::HintIter;
+
+	for t in &["rawmidi"] {
+		println!("{} devices:", t);
+		let i = HintIter::new(None, &*CString::new(*t).unwrap()).unwrap();
+		for a in i { println!("  {:?}", a) }
+	}
+	list_cards();
+	*/
+	let mio = Rawmidi::new("hw:mio", Direction::input(), false).unwrap();
+	for data in mio.io().bytes() {
+		println!("{}", data.unwrap());
+	}
+}
